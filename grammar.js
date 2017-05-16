857ea439a685e9a2443197a76b95fc929c0383d8
fallax <fallaburr@gmail.com>
2017-05-16 11:09:55 +0100

    Create grammar.js

---
+++ grammar.js
@@ -0,0 +1,97 @@
+var walandablap = {
+	"origin": [
+		"#pattern1#"
+	],
+	"pattern1": [
+		"#consmess##vowel##consend#",
+		"#consmess##vowel##double##afterdouble#",
+		"#consmess##pair##afterpair#",
+		"#rhythmic#",
+		"#beatcon#al#rhythmic#"
+	],
+	"consmess": [
+		"gl",
+		"spl",
+		"vr",
+		"gr",
+		"n",
+		"kl",
+		"tr",
+		"pr",
+		"pl",
+		"ft",
+		"sm",
+		"sw",
+		"br",
+		"spr",
+		"gn",
+		"bl"
+	],
+	"consend": [
+		"g",
+		"b",
+		"nt",
+		"rt",
+		"lp",
+		"rk",
+		"t",
+		"#double#",
+		"lts",
+		"ck",
+		"nk"
+	],
+	"double": [
+		"dd",
+		"bb",
+		"gg",
+		"ff",
+		"tt"
+	],
+	"afterdouble": [
+		"le",
+		"a#suffix#",
+		"id",
+		"is",
+		"ie"
+	],
+	"vowel": [
+		"e",
+		"o"
+	],
+	"pair": [
+		"oi",
+		"ee",
+		"oo"
+	],
+	"afterpair": [
+		"k",
+		"d",
+		"l",
+		"b"
+	],
+	"suffix": [
+		"lop",
+		"lok",
+		"lod",
+		"lob"
+	],
+	"rhythmic": [
+		"u#beatcon#tu#beatcon#",
+		"i#beatcon#ni#beatcon#",
+		"o#beatcon#no#beatcon#",
+		"e#beatcon#ne#beatcon#",
+		"abi#beatcon#ni#beatcon#",
+		"t#joinvowel#rri#beatcon#",
+		"#beatcon#l#joinvowel##beatcon#"
+	],
+	"beatcon": [
+		"b",
+		"g",
+		"p",
+		"d"
+	],
+	"joinvowel": [
+		"u",
+		"a"
+	]
+}
